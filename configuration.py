216602c3bc31976fd4b2cebd53a60ea9b8f421b6
lamarch <paulmarchal54370@gmail.com>
2021-06-22 11:02:14 +0200

    Configuration 2

--- configuration.py
+++ configuration.py
@@ -17,10 +17,36 @@
 """
 
 
-class Configuration:
+CONFIG_DEFAUT = """
+{
+    "matières":
+    [
+        ["Fumier bovins","Fumier bovins C2"],
+        ["Fumier bovins mou","Fumier bovins mou C2"],
+        ["Lisiers","Lisiers C2"],
+        ["Fumier ovins","Fumier ovins C2"],
+        ["Maïs ensilage"],
+        ["Seigle ensilage"],
+        ["Paille"],
+        ["DIGESTAT","DIGESTAT C2"],
+    ],
+    "decalages":{
+        "haut":7,
+        "gauche":9,
+        "entre_clients":1
+    }
+}
+"""
+
+
+class ChargeurConfiguration:
     def __init__(self, path: str) -> None:
         self.path = path
 
-    def load(self):
+    def charger(self):
         from json import load
         return load(self.path)
+
+    def defaut(self):
+        from json import loads
+        return loads(CONFIG_DEFAUT)
