df575d1a0524b2d5751e43657942a56eb8373d2e
lamarch <paulmarchal54370@gmail.com>
2021-06-22 11:42:57 +0200

    src folder

--- src/tests.py
+++ src/tests.py
@@ -2,15 +2,12 @@ from json import dumps
 
 
 def run_input():
-    import input
-    import main
 
     chargeur = input.Chargeur(main.FICHIER_IN)
     print(dumps(chargeur.recuperer()))
 
 
 def run_main():
-    import main
 
 
 if __name__ == "__main__":
